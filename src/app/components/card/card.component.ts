1f95afb97fbf18f0216599a7dbf5295171efbcd1
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-10-31 19:19:06 -0300

    adicionado environment

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  name: string = 'CHARMANDER';
+  attributeTypes: string[] = ['Fire', 'Fly'];
 }
